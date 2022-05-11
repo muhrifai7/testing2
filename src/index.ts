5a573fe225a0dbd57cae9ac98fba2ef2ce9f0575
muh rifai <muhrifai554@gmail.com>
2022-05-11 18:43:08 +0700

    test 1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-import express from "express";
+import "dotenv/config";
+import "reflect-metadata";
+import fs from "fs";
 import path from "path";
 
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+
+import { errorHandler } from "./middleware/errorHandler";
+import { getLanguage } from "./middleware/getLanguage";
+import routes from "./routes";
+// import { dbCreateConnection } from "./typeorm/dbCreateConnection";
+
 export const app = express();
-const port = process.env.PORT || 4000;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(getLanguage);
+
+// app.use("/", routes);
 
 app.use("/", (req, res) => res.send(`oke ${port}`));
 
+app.use(errorHandler);
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Server runnings  on port ${port}`);
+  console.log(`Server running tetete  on port ${port}`);
 });
+
+// (async () => {
+//   await dbCreateConnection();
+// })();
