c4152bf85ceb80dd44b33260f9533b882adad773
muh rifai <muhrifai554@gmail.com>
2022-05-11 18:22:32 +0700

    oke

--- index.ts
+++ index.ts
@@ -1,27 +1,12 @@
-import bodyParser from "body-parser";
-// import cors from "cors";
 import express from "express";
+import path from "path";
 
-// import { errorHandler } from "./middleware/errorHandler";
-// import { getLanguage } from "./middleware/getLanguage";
-// import routes from "./routes";
-// import { dbCreateConnection } from "./typeorm/dbCreateConnection";
+const PORT = process.env.PORT || 5000;
+const server = express();
 
-export const app = express();
-// app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(getLanguage);
-
-const port = process.env.PORT || 4000;
-app.use("/", (req, res) => res.send(`okeee ${port}`));
-
-// app.use(errorHandler);
-
-app.listen(port, () => {
-  console.log(`Server running  on port ${port}`);
-});
-
-// (async () => {
-//   await dbCreateConnection();
-// })();
+express()
+  .use(express.static(path.join(__dirname, "../public")))
+  .set("views", path.join(__dirname, "../views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
