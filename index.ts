8fc331342e15e301b0468274c50d401c01ebd896
muh rifai <muhrifai554@gmail.com>
2022-05-11 18:33:08 +0700

    test 1

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import path from "path";
 
-const PORT = process.env.PORT || 5000;
-const server = express();
+export const app = express();
+const port = process.env.PORT || 4000;
 
-express()
-  .use(express.static(path.join(__dirname, "../public")))
-  .set("views", path.join(__dirname, "../views"))
-  .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.use("/", (req, res) => res.send(`oke ${port}`));
+
+app.listen(port, () => {
+  console.log(`Server running  on port ${port}`);
+});
