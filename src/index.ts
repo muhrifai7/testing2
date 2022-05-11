d7a2fb4cd6b249511ad45c204154a12ffce00151
muh rifai <muhrifai554@gmail.com>
2022-05-11 19:07:12 +0700

    test 1

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,15 @@ import cors from "cors";
 import express from "express";
 
 import { errorHandler } from "./middleware/errorHandler";
-import { getLanguage } from "./middleware/getLanguage";
-import routes from "./routes";
+// import { getLanguage } from "./middleware/getLanguage";
+// import routes from "./routes";
 // import { dbCreateConnection } from "./typeorm/dbCreateConnection";
 
 export const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(getLanguage);
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(getLanguage);
 
 // app.use("/", routes);
 
